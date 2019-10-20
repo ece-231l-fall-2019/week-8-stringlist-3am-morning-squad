7f13e3c82b361c12415e72193f53bccef60115b1
Jack LaMarre <jlamarre0802@unm.edu>
2019-10-19 18:00:27 -0600

    week-8

--- Stringlist.cpp
+++ Stringlist.cpp
@@ -39,12 +39,12 @@ void push_back(string str) {
 	ptr -> str = str;
 	ptr -> next = 0;
 	ptr ->prev = back;
-	if (back != 0)
-	
-		back->next = ptr;
-	else
-		front = ptr;
-back = ptr;
+		if (back != 0)
+			back->next = ptr;
+		else
+			front = ptr;
+	back = ptr;
+	size++;	
 }
 void push_front(string str){
 	llist *ptr = new llist;
@@ -56,7 +56,49 @@ void push_front(string str){
 		else
 			back = ptr;
 	front = ptr;
+	size++;
+}
+void pop_front()
+{
+	llist ptr = saveptr = front;
+	front = front ->next;
+		if (front)
+			front ->prev = front -> prev -> prev;
+		else
+			back = 0;
+	delete saveptr;
+	size--;
+}
+void pop_back()
+{
+	llist ptr = saveptr = back;
+	back = back -> prev;
+		if (back)
+			back -> next = back ->next -> next;
+		else
+			front = 0;
+	delete saveptr;
+	size--;
+}		
+bool empty() 
+{
+
+}
+void uniq()
+{
+	for(llist *ptr = front; ptr != 0; ptr = ptr ->next)
+	{
+		while(ptr -> next != 0 && ptr -> str = ptr -> next -> next)
+		{
+			llist ptr = saveptr = ptr -> next;
+			ptr -> next = ptr -> next -> next;
+			if(saveptr -> next != 0;)
+				saveptr -> next -> prev = ptr;
+			else
+				back = ptr;
+		delete saveptr;
+		size--;
+		}
+	}
 }
 
-bool empty() 
-}
